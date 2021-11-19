35802a98924b2225b2eb8d83f55f50b9280ca439
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-19 15:37:04 +0100

    improved messages in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,15 @@ export MY_IP=`pinky | grep root | awk '{ print $6 }'`
 
 cd ~
 
+echo "Transfer needed files from the client"
 scp asolimando@$MY_IP:'/Users/asolimando/Documents/advisor/lib.zip /Users/asolimando/git/workload-insights/wi /Users/asolimando/git/workload-insights/target/workload-insights-1.0-SNAPSHOT.jar' .
 
+echo "Set variables"
 export PATH=$PATH:/usr/java/jdk1.8.0_232-cloudera/bin
 export WI_HOME=~
 
+echo "Unpack libraries and cleanup"
 unzip lib.zip
 rm lib.zip
 
-echo Setup completed
+echo "Setup completed!"
