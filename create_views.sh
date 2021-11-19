852c2b61d23964c453fafa0b81c2b1893dc08a21
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-19 16:46:14 +0100

    fixed quoting

--- create_views.sh
+++ create_views.sh
@@ -2,4 +2,4 @@
 db_name=${db_name:-"tpcds_bin_partitioned_orc_2"}
 FILE=${1:-"~/out_dir/ALL-FULL.json"}
 
-beeline -n hive -u 'jdbc:hive2://localhost:2181/$db_name;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2?tez.queue.name=default' -e "`jq -r .recommended_views[].sql < $FILE`"
+beeline -n hive -u "jdbc:hive2://localhost:2181/$db_name;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2?tez.queue.name=default" -e "`jq -r .recommended_views[].sql < $FILE`"
