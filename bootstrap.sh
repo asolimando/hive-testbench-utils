7f87b78cb6dd4530a17df0c2b54cb1ea76ba2806
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-19 16:49:36 +0100

    change wi client location

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ export MY_IP=`pinky | grep root | awk '{ print $6 }'`
 cd ~
 
 echo "Transfer needed files from the client"
-scp asolimando@$MY_IP:'/Users/asolimando/Documents/advisor/lib.zip /Users/asolimando/git/workload-insights/wi /Users/asolimando/git/workload-insights/target/workload-insights-1.0-SNAPSHOT.jar' .
+scp asolimando@$MY_IP:'/Users/asolimando/Documents/advisor/lib.zip /Users/asolimando/Documents/advisor/wi /Users/asolimando/git/workload-insights/target/workload-insights-1.0-SNAPSHOT.jar' .
 
 echo "Set variables"
 export PATH=$PATH:/usr/java/jdk1.8.0_232-cloudera/bin
