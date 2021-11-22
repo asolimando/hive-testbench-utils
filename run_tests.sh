68183f3fb6baee43abf6a72d332cf139d2544e8f
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-22 18:13:57 +0100

    updated run test folder

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,8 @@ DATASET=${1:-""}
 SCALE=${2:-2}
 REPETITIONS=${3:-10}
 
+cd ~/hive-testbench
+
 for i in {1..$REPETITIONS}; do
   echo Start run $i
   ./runSuite.pl $DATASET $SCALE > ~/$DATASET_$SCALE_run_$i.csv
