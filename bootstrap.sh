66cab2229392bb162fd2ce3528d0010ae0b0a7a7
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-22 17:06:51 +0100

    fixed script invocation bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,8 @@ cd ~
 echo "Transfer needed files from the client"
 scp asolimando@$MY_IP:'/Users/asolimando/Documents/advisor/lib.zip /Users/asolimando/Documents/advisor/wi /Users/asolimando/git/workload-insights/target/workload-insights-1.0-SNAPSHOT.jar' .
 
-echo "Set variables"
+echo "Setting JDK"
 export PATH=$PATH:/usr/java/jdk1.8.0_232-cloudera/bin
-export WI_HOME=~
 
 echo "Unpack libraries and cleanup"
 unzip lib.zip
@@ -19,7 +18,8 @@ echo "Clone hive-testbench project"
 git clone https://github.com/asolimando/hive-testbench.git
 
 echo "Building hive-testbench binaries"
-~/hive-testbench/tpcds-build.sh
+cd ~/hive-testbench
+./tpcds-build.sh
 
 echo "Setup completed!"
 echo "Now create data by running: ~/hive-testbench/tpcds-setup.sh $SCALE"
