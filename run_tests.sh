34cec259932885fa256852002d00a6e18682d9bc
Alessandro Solimando <alessandro.solimando@gmail.com>
2021-11-22 18:18:27 +0100

    iterations fixed

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ REPETITIONS=${3:-10}
 
 cd ~/hive-testbench
 
-for i in {1..$REPETITIONS}; do
+for i in `seq 1 $REPETITIONS`; do
   echo Start run $i
   ./runSuite.pl $DATASET $SCALE > ~/$DATASET_$SCALE_run_$i.csv
   echo End run $i
